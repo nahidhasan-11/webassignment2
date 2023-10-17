faa1bd61c12410121f9a4124a8537c0ba74e6160
Md. Nahid Hasan <143954415+nahidhasan-11@users.noreply.github.com>
2023-10-17 04:58:49 -0800

    Update Main.php

--- Main.php
+++ Main.php
@@ -1,33 +1,16 @@
 <?php
-require_once 'Book.php';
-require_once 'Customer.php';
 
-// Create a Book object
-$book = new Book(123456, 'Sample Book', 'John Doe', 5);
+    require_once './Book.php';
+    require_once './Customer.php';
 
-// Create a Customer object
-$customer = new Customer(1, 'Alice', 'Johnson', 'alice@example.com');
 
-// Use getters and setters
-$book->setTitle('New Title');
-$book->setAvailable(10);
-$customer->setEmail('newemail@example.com');
+    $book1 = new Book("1234", "Hello Bangladesh", "Hasnain", 150);
+    $customer1 = new Customer(123, "Hasnain", "Ali", "mdhasnainali.01@gmail.com");
 
-// Use dynamic getters and setters
-echo "Book Title: " . $book->getTitle() . "\n";
-echo "Customer Email: " . $customer->getEmail() . "\n";
+    echo $book1->__toString();
+    echo $customer1->__toString();
 
-// Check if you can get a copy of the book
-if ($book->getCopy()) {
-    echo "You can get a copy of the book.\n";
-} else {
-    echo "The book is out of stock.\n";
-}
+    
+    echo "</br>".$book1->getCopy();
 
-// Add copies of the book
-$book->addCopy(3);
-
-// Display the book and customer information
-echo "Book Information:\n" . $book . "\n";
-echo "Customer Information:\n" . $customer . "\n";
 ?>
