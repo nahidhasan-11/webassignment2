a911f956e493e25ce6afa3d60de15a271138ace5
Md. Nahid Hasan <143954415+nahidhasan-11@users.noreply.github.com>
2023-10-17 05:00:02 -0800

    Update Main.php

--- Main.php
+++ Main.php
@@ -3,14 +3,21 @@
     require_once './Book.php';
     require_once './Customer.php';
 
-
-    $book1 = new Book("1234", "Hello Bangladesh", "Hasnain", 150);
-    $customer1 = new Customer(123, "Hasnain", "Ali", "mdhasnainali.01@gmail.com");
-
-    echo $book1->__toString();
-    echo $customer1->__toString();
-
+   
+    $book1 = new Book("1234", "Sample Book", "Rahim", 150);
+    $customer1 = new Customer(123, "Nusrat", "Jannat", "nusratjannat2001@gmail.com");
     
-    echo "</br>".$book1->getCopy();
+   // $book2 = new Book("5678", "Story Book", "Karim",50);
+   // $customer2 = new Customer(567, "Montasir", "Munem", "montasirmunem01@gmail.com");
+    echo "<h1> Information of Books </h1>";
+    echo $book1->__toString();
+    echo "<h1> Information of Customers </h1>";
+    echo $customer1->__toString();
+    //echo "</br>";
+   // echo $book2->__toString();
+   // echo $customer2->__toString();
+    
+   // echo "</br>".$book1->getCopy();
+    
 
 ?>
